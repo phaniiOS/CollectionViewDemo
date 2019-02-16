4aadd3c1018c761dd9957d97698071821cfdb19e
Phani <phanindranath.wg113@gmail.com>
2019-02-16 11:22:13 -0600

    Collection view for images

---
+++ CollectionViewDemo/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewDemo
+//
+//  Created by IMCS2 on 2/15/19.
+//  Copyright © 2019 IMCS2. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var CellImageView: UIImageView!
+    
+}
